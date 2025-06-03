91f281563cde93e472b0bb66c40dbc280885dad7
KrishCodesw <krishjain.w@gmail.com>
2025-06-03 19:36:07 +0530

    Entire POSTGRESQL Notes with explanation

--- Post/dist/index.js
+++ Post/dist/index.js
@@ -22,18 +22,66 @@ app.post("/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* ()
     const username = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
+    const city = req.body.city;
+    const country = req.body.country;
     try {
-        const insertQuery = `INSERT INTO users (username,email,password) VALUES ('${username}','${email}','${password}')`;
-        const response = yield pgClient.query(insertQuery);
+        const insertQuery = `INSERT INTO users (username,email,password) VALUES ($1,$2,$3) RETURNING id;`; /// This is to avoid SQL Injections 
+        yield pgClient.query("BEGIN;");
+        const response = yield pgClient.query(insertQuery, [username, email, password]); ///-------------------- Query 1 
+        const user_id = response.rows[0].id;
+        const InsertAddressQuery = `INSERT INTO addresses (city,country,user_id) VALUES ($1,$2,$3);`;
+        const addressresponse = yield pgClient.query(InsertAddressQuery, [city, country, user_id]); ///-------------------- Query 2 
+        yield pgClient.query("COMMIT;");
+        // IF query 1 runs and the Back in crashes the query 2 will not run partial user will be created without an addressso to avoid this we have to use transactions Do we send two SQL queries into the database? What if one of the queries (address query for example) fails? 
+        // This would require transactions  in SQL to ensure either both the user information and address goes in, or neither does
+        //-----------------------------------------------------------------------------------------------------------------------------------------------
+        //What would the SQL query look like under the hood ?
+        // BEGIN; -- Start transaction
+        // INSERT INTO users (username, email, password)
+        // VALUES ('john_doe', 'john_doe1@example.com', 'securepassword123');
+        // INSERT INTO addresses (user_id, city, country, street, pincode)
+        // VALUES (currval('users_id_seq'), 'New York', 'USA', '123 Broadway St', '10001');
+        // COMMIT;
+        //-----------------------------------------------------------------------------------------------------------------------------------------------
+        //                                                  JOINS 
+        // 1. BAD APPROACH
+        // -- Query 1: Fetch user's details
+        // SELECT id, username, email
+        // FROM users
+        // WHERE id = YOUR_USER_ID;
+        // -- Query 2: Fetch user's address
+        // SELECT city, country, street, pincode
+        // FROM addresses
+        // WHERE user_id = YOUR_USER_ID;
+        // 2.GOOD APPROACH
+        // SELECT users.id, users.username, users.email, addresses.city, addresses.country, addresses.street, addresses.pincode
+        // FROM users JOIN addresses ON users.id = addresses.user_id
+        // WHERE users.id = '1';
+        // SELECT u.id, u.username, u.email, a.city, a.country, a.street, a.pincode
+        // FROM users u
+        // JOIN addresses a ON u.id = a.user_id
+        // WHERE u.id = YOUR_USER_ID;
+        //-----------------------------------------------------------------------------------------------------------------------------------------------
         res.json({
             message: "You have signed up"
         });
     }
     catch (e) {
+        console.error("Actual error:", e);
         res.json({
             message: "Error signing up"
         });
     }
+}));
+app.get("/metadata", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = req.query.id;
+    const query = `SELECT users.id, users.username, users.email, addresses.city, addresses.country
+FROM users JOIN addresses ON users.id = addresses.user_id
+WHERE users.id = ${id};`;
+    const response = yield pgClient.query(query);
+    res.json({
+        response: response.rows
+    });
 }));
 app.listen(3000);
 // const pgClient2=new Client({
